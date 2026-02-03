4c3e0b56c945452c87b8ce687e04ce739d0583dc
Hanna Pan <hzpan8@uw.edu>
2026-02-02 22:14:39 -0800

    create new player struct

--- LifeCounter/ContentView.swift
+++ LifeCounter/ContentView.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 
+struct Player: Identifiable {
+    let id = UUID()
+    var name: String
+    var life: Int
+}
+
 struct ContentView: View {
-    @State private var player1Life = 20
-    @State private var player2Life = 20
+//    @State private var player1Life = 20
+//    @State private var player2Life = 20
+    @State private var players: [Player] = [
+        Player(name: "Player 1", life: 20),
+        Player(name: "Player 2", life: 20),
+        Player(name: "Player 3", life: 20),
+        Player(name: "Player 4", life: 20)
+    ]
+    @State private var startGame = false
     
     var body: some View {
         GeometryReader { geometry in
@@ -20,19 +33,28 @@ struct ContentView: View {
                     if (isLandscape) {
                         // horizontal --> side by side player view
                         HStack(spacing: 12) {
-                            playerViewPanel(name: "Player 1", life: $player1Life)
-                            playerViewPanel(name: "Player 2", life: $player2Life)
+                            ForEach(players.indices, id: \.self) { i in
+                                playerViewPanel(name: players[i].name, life: $players[i].life)
+                            }
+                            
+                            // playerViewPanel(name: "Player 1", life: $player1Life)
+                            // playerViewPanel(name: "Player 2", life: $player2Life)
                         }
                     } else {
                         // vertical --> top down player view
                         VStack(spacing: 12) {
-                            playerViewPanel(name: "Player 1", life: $player1Life)
-                            playerViewPanel(name: "Player 2", life: $player2Life)
+                            ForEach(players.indices, id: \.self) { i in
+                                playerViewPanel(name: players[i].name, life: $players[i].life)
+                            }
+                            
+                            // playerViewPanel(name: "Player 1", life: $player1Life)
+                            // playerViewPanel(name: "Player 2", life: $player2Life)
                         }
                     }
                 }
                 .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
-                Text(winText(player1: player1Life, player2: player2Life))
+                // Text(winText(player1: player1Life, player2: player2Life))
+                Text(winText())
                     .font(.headline)
                     .padding(.vertical, 20)
                     .padding(.bottom, 40)
@@ -85,12 +107,17 @@ struct ContentView: View {
     }
     
     
-    func winText(player1: Int, player2: Int) -> String {
-        if (player1 <= 0) {
-            return "Player 1 LOSES!"
-        }
-        if (player2 <= 0) {
-            return "Player 2 LOSES!"
+    func winText() -> String {
+//        if (player1 <= 0) {
+//            return "Player 1 LOSES!"
+//        }
+//        if (player2 <= 0) {
+//            return "Player 2 LOSES!"
+//        }
+        for i in players.indices {
+            if (players[i].life <= 0) {
+                return "\(players[i].name) LOSES!"
+            }
         }
         return ""
     }
